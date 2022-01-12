3a8eb160897a4a55b5f1d9d5b81eec9f728335dd
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-01-12 15:31:10 -0300

    finalizacao do banner na pagina de home

---
+++ src/components/Banner.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id ="main-banner">
+        <h1>Monte seu Açaí</h1>
+    </div>
+
+</template>
+
+<script>
+export default{
+    name: 'Banner'
+}
+
+</script>
+
+<style scoped>
+    #main-banner{
+        background-image: url('/img/acai.png');
+        background-size: cover;
+        background-position: 0;
+        height: 650px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+
+    }
+
+    #main-banner h1{
+        color: #FFF;
+        font-size: 60px;
+        text-align: center;
+        padding: 20px 40px;
+        background-color: #4C2D51
+    }
+
+</style>
